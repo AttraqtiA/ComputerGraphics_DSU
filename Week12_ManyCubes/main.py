d8cac6914281275723df4cda387724c721c23dd8
Your Name <samuelmiracle.lie@gmail.com>
2024-12-05 11:07:21 +0900

    Imported obj file for Final Pokemon Game project

--- Week12_ManyCubes/main.py
+++ Week12_ManyCubes/main.py
@@ -1,12 +1,12 @@
 import pygame
-import OpenGL
 from pygame.locals import *
+
 from OpenGL.GL import *
 from OpenGL.GLU import *
-import random
-import numpy
 
-vertices= (
+import random
+
+vertices = (
     (1, -1, -1),
     (1, 1, -1),
     (-1, 1, -1),
@@ -15,57 +15,82 @@ vertices= (
     (1, 1, 1),
     (-1, -1, 1),
     (-1, 1, 1)
-    )
+)
 
-# Bikin line"nya yg membentuk cube
 edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-    )
+    (0, 1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7)
+)
+
+surfaces = (
+    (0, 1, 2, 3),
+    (3, 2, 7, 6),
+    (6, 7, 5, 4),
+    (4, 5, 1, 0),
+    (1, 5, 7, 2),
+    (4, 0, 3, 6)
+)
 
 colors = (
-    (1,0,0),
-    (0,1,0),
-    (0,0,1),
-    (0,1,0),
-    (1,1,1),
-    (0,1,1),
-    (1,0,0),
-    (0,1,0),
-    (0,0,1),
-    (1,0,0),
-    (1,1,1),
-    (0,1,1),
-    )
+    (1, 0, 0),
+    (0, 1, 0),
+    (0, 0, 1),
+    (0, 1, 0),
+    (1, 1, 1),
+    (0, 1, 1),
+    (1, 0, 0),
+    (0, 1, 0),
+    (0, 0, 1),
+    (1, 0, 0),
+    (1, 1, 1),
+    (0, 1, 1),
+)
 
-# Jadi gk cuman edge aja, ini kita bikin SISI" nya
-surfaces = (
-    (0,1,2,3),
-    (3,2,7,6),
-    (6,7,5,4),
-    (4,5,1,0),
-    (1,5,7,2),
-    (4,0,3,6)
-    )
 
-def Cube():
+def set_vertices(max_distance):
+    x_value_change = random.randrange(-10, 10)
+    y_value_change = random.randrange(-10, 10)
+    z_value_change = random.randrange(-1 * max_distance, -20)
+
+    new_vertices = []
+
+    for vert in vertices:
+        new_vert = []
+
+        new_x = vert[0] + x_value_change
+        new_y = vert[1] + y_value_change
+        new_z = vert[2] + z_value_change
+
+        new_vert.append(new_x)
+        new_vert.append(new_y)
+        new_vert.append(new_z)
+
+        new_vertices.append(new_vert)
+
+    return new_vertices
+
+
+def Cube(vertices):
     glBegin(GL_QUADS)
+
     for surface in surfaces:
         x = 0
+
         for vertex in surface:
             x += 1
             glColor3fv(colors[x])
             glVertex3fv(vertices[vertex])
+
     glEnd()
 
     glBegin(GL_LINES)
@@ -74,22 +99,31 @@ def Cube():
             glVertex3fv(vertices[vertex])
     glEnd()
 
+
 def main():
     pygame.init()
-    display = (800,600)
-    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
 
-    # glTranslatef(0.0, 0.0, -5)
-    glTranslatef(random.randrange(-5,5),0, -30)
+    glTranslatef(random.randrange(-5, 5), random.randrange(-5, 5), -40)
 
-    # no more rotate
+    # object_passed = False
+
+    x_move = 0
+    y_move = 0
+
+    max_distance = 100
+
+    cube_dict = {}
+
+    for x in range(20):
+        cube_dict[x] = set_vertices(max_distance)
+
     # glRotatef(25, 2, 1, 0)
 
-    object_passed = False
-
-    while not object_passed:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -97,39 +131,49 @@ def main():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    glTranslatef(-0.5,0,0)
+                    x_move = 0.3
                 if event.key == pygame.K_RIGHT:
-                    glTranslatef(0.5,0,0)
+                    x_move = -0.3
 
                 if event.key == pygame.K_UP:
-                    glTranslatef(0,1,0)
+                    y_move = -0.3
                 if event.key == pygame.K_DOWN:
-                    glTranslatef(0,-1,0)
+                    y_move = 0.3
 
-            # if event.type == pygame.MOUSEBUTTONDOWN:
-            #     if event.button == 4:
-            #         glTranslatef(0,0,1.0)
-            #
-            #     if event.button == 5:
-            #         glTranslatef(0,0,-1.0)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_move = 0
+
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    y_move = 0
+
+        #            if event.type == pygame.MOUSEBUTTONDOWN:
+        #                if event.button == 4:
+        #                    glTranslatef(0,0,1.0)
+        #
+        #                if event.button == 5:
+        #                    glTranslatef(0,0,-1.0)
+
+        # glRotatef(1, 3, 1, 1)
 
         x = glGetDoublev(GL_MODELVIEW_MATRIX)
+        # print(x)
+
         camera_x = x[3][0]
         camera_y = x[3][1]
         camera_z = x[3][2]
 
-        if camera_z <= 0:
-            object_passed = True
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        # slowly move
-        glTranslatef(0,0,0.5)
+        glTranslatef(x_move, y_move, .50)
 
-        # glRotatef(1, 3, 1, 1)
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        Cube()
+        for each_cube in cube_dict:
+            Cube(cube_dict[each_cube])
+
         pygame.display.flip()
         pygame.time.wait(10)
 
 
-for i in range(10000):
-    main()
+main()
+pygame.quit()
+quit()
